5d127fdd92e190f6cd22ef30e8c838e195946a67
cloakzzzy <cloakzybruh@gmail.com>
2022-10-17 16:21:49 +0100

    latest commit

--- Camera.h
+++ Camera.h
@@ -3,13 +3,14 @@
 
 class Camera {
 public:
-	int width; int height; int fov; int focal_length; int x; int y; int z;
+	int width; int height; int fov; int focal_length; int x; int y; int z; int degx;
 
-	Camera(int Width, int Height, int Fov = 60, int X = 0, int Y = 0, int Z = 0)
+	Camera(int Width, int Height, int Fov = 60, int X = 0, int Y = 0, int Z = 0, int Degx = 0)
 	{
 		width = Width; height = Height;
 		fov = Fov;
 		focal_length = round((width / 2) / tan((fov / 2) * RAD));
 		x = X; y = Y; z = Z;
+		degx = Degx;
 	}
 };
